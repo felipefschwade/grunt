84bd7cda0ee68f5fba495218a8bc2ae2afdea58c
felipefschwade <felipefaeschwade@gmail.com>
2015-11-16 22:20:06 -0300

    First Commit

---
+++ sintaxes-Grunt.js
@@ -0,0 +1,46 @@
+Para iniciar o grunt:
+module.exports = function(grunt){
+    grunt.initCongi({
+        plugin1: {config},
+        plugin2: {config}
+    });
+}
+Para carregar outras NPms : 
+Para utilizar a task e a target no terminal: grunt task:target
+Utilizando o Copy:
+    module.exports = function(grunt){
+    grunt.initCongi({
+        copy:{
+            pasta:{
+                expand: true,
+                cwd: 'pasta',
+                src: '**', //** indica todos
+                dest: 'dist'
+            }
+        },
+        clean{
+            dist:{
+                src: 'dist'
+            }
+        }
+    });
+        grunt.loadNpmTasks('grunt-contri-copy');
+Utilizando o Clean:
+        module.exports = function(grunt){
+            grunt.initCongi({
+                copy:{
+                    pasta:{
+                        expand: true,
+                        cwd: 'pasta',
+                        src: '**', //** indica todos
+                        dest: 'dist'
+                    }
+                }
+            });
+                grunt.loadNpmTasks('grunt-contrib-copy');
+                grunt.loadNpmTasks('grunt-contrib-clean');
+            
+Para adicioanr uma nova task: grunt.registerTask('nome' , ['nometaskconfigurada', 'nometaskconfigurada']);
+Execução: grunt task;
+Para adicionar uma task ao default: grunt.registerTask('default', ['task']);
+Execução: grunt;
